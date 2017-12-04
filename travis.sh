8fe643f55902e937ef1f0d4639df24a2ee53ed4d
Frank Terragna <fterragna@gmail.com>
2017-12-04 01:15:20 -0500

    Fail the build if go test or goveralls encounters an error

--- travis.sh
+++ travis.sh
@@ -4,8 +4,8 @@ docker build -t app-test --target builder . || exit 1
 
 # Run the app test container and publish test coverage to Coveralls.
 CONTAINER_ID=$(docker run -d app-test tail -f /dev/null) || exit 1
-docker exec $CONTAINER_ID go test -v ./...
-docker exec $CONTAINER_ID goveralls -repotoken $COVERALLS_TOKEN
+docker exec $CONTAINER_ID go test -v ./... || exit 1
+docker exec $CONTAINER_ID goveralls -repotoken $COVERALLS_TOKEN || exit 1
 
 docker stop $CONTAINER_ID
 
